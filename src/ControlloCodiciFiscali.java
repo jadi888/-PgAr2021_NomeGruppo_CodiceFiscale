6dc37e9ad01b0677f8d4019bc02ed663039abf73
souhailamouak <s.mouak@studenti.unibs.it>
2021-04-30 12:19:59 +0200

    ho caricato il 3 punto ma non l'ho ancora finito

--- src/ControlloCodiciFiscali.java
+++ src/ControlloCodiciFiscali.java
@@ -75,21 +75,12 @@ public class ControlloCodiciFiscali {
                }
 
 
-               if ((codice.charAt(8) == 'A') || (codice.charAt(8) == 'B') ||
-                       (codice.charAt(8) == 'C') || (codice.charAt(8) == 'D') ||
-                       (codice.charAt(8) == 'E') || (codice.charAt(8) == 'H') ||
-                       (codice.charAt(8) == 'L') || (codice.charAt(8) == 'M') ||
-                       (codice.charAt(8) == 'P') || (codice.charAt(8) == 'R') ||
-                       (codice.charAt(8) == 'S') || (codice.charAt(8) == 'T'))
-                   corretto = true; //controllo la validità del mese
-               else
-                   return false;
 
                int giorno;
                if (Character.isDigit(codice.charAt(9)) && Character.isDigit(codice.charAt(10))) {
-                   giorno = Integer.parseInt(codice.substring(9, 11));
-                   if ((giorno >= 1 && giorno <= 31) || (giorno >= 41 && giorno <= 71))
-                       corretto = true;
+                   String EstraiMeseGiorno = String.valueOf(codice).substring(8,11);
+                   if(controlloMeseGiorno(EstraiMeseGiorno))
+                       corretto=true;
                    else
                        return false; //controllo che il giorno di nascita sia costituito da cifre
                } else
@@ -118,120 +109,28 @@ public class ControlloCodiciFiscali {
                        return false;
                }
 
-               return true;
+        CodiceFiscale
 
-               //controllo ultimo carattere
-
-        int codice_finale;
-        int totale = 0;
-        int totale_pari=0;
-        int totale_dispari=0;
+        return corretto;
 
 
-        /* le stringhe in cui salvo rispettivamente i caratteri con indice pari o dispari*/
-        String pari = "";
-        String dispari = "";
-        for (int j = 0; j < cod.length(); j++) {
-            if (j % 2 == 0) {
-                pari += cod.charAt(j);
-            } else {
-                dispari += cod.charAt(j);
-            }
-        }
 
-        //per i caratteri con indice pari procedo nel seguente modo:
-        final String alphabet = "ABCDEFGHIJKLMNOPQRST";
-        for(int i = 0; i < pari.length(); i++){
-            /*se il carattere è un intero lo sommo al totale dei pari*/
-            if(Character.isDigit(pari.charAt(i)))
-                totale_pari+=pari.indexOf(i);
-                /*se il carattere è un char allora li assegno il numero in base all'ordine che occupa nell'alfabeto*/
-            else
-                totale_pari+=(alphabet.indexOf(pari.charAt(i)));
-        }
+    }
 
-        //per i caratteri con indice DISPARI procedo nel seguente modo:
-        for(int i=0; i < dispari.length(); i++){
-            //verifico se in quella posizione c'è un intero, 0 o 1;
-            if(Character.isDigit(dispari.charAt(i)) && dispari.indexOf(i) != 0 && dispari.indexOf(i) != 1){
-                if(dispari.indexOf(i)%2==0)
-                    totale_dispari += dispari.indexOf(i)+1;
-            }
-            else if(dispari.charAt(i)=='1')
-                totale_dispari+=0;
-            else if(dispari.charAt(i) == '0')
-                totale_dispari+=1;
-            else if(dispari.charAt(i)=='A')
-                totale_dispari+=1;
-            else if(dispari.charAt(i)=='B')
-                totale_dispari+=0;
-            else if(dispari.charAt(i)=='C')
-                totale_dispari+=5;
-            else if(dispari.charAt(i)=='D')
-                totale_dispari+=7;
-            else if(dispari.charAt(i)=='E')
-                totale_dispari+=9;
-            else if(dispari.charAt(i)=='F')
-                totale_dispari+=13;
-            else if(dispari.charAt(i)=='G')
-                totale_dispari+=15;
-            else if(dispari.indexOf(i)=='H')
-                totale_dispari+=17;
-            else if(dispari.indexOf(i)=='I')
-                totale_dispari+=19;
-            else if(dispari.indexOf(i)=='J')
-                totale_dispari+=21;
-            else if(dispari.charAt(i)=='K')
-                totale_dispari+=2;
-            else if(dispari.charAt(i)=='L')
-                totale_dispari+=4;
-            else if(dispari.charAt(i)=='M')
-                totale_dispari+=18;
-            else if(dispari.charAt(i)=='N')
-                totale_dispari+=20;
-            else if(dispari.charAt(i)=='O')
-                totale_dispari+=11;
-            else if(dispari.charAt(i)=='P')
-                totale_dispari+=3;
-            else if(dispari.charAt(i)=='Q')
-                totale_dispari+=6;
-            else if(dispari.charAt(i)=='R')
-                totale_dispari+=8;
-            else if(dispari.charAt(i)=='S')
-                totale_dispari+=12;
-            else if(dispari.charAt(i)=='T')
-                totale_dispari+=14;
-            else if(dispari.charAt(i)=='U')
-                totale_dispari+=16;
-            else if(dispari.charAt(i)=='V')
-                totale_dispari+=10;
-            else if(dispari.charAt(i)=='W')
-                totale_dispari+=22;
-            else if(dispari.charAt(i)=='X')
-                totale_dispari+=25;
-            else if(dispari.charAt(i)=='Y')
-                totale_dispari+=24;
-            else if(dispari.charAt(i)=='Z')
-                totale_dispari+=23;
-        }
+    private static boolean controlloMeseGiorno(String carattere) {
+        char primoEl = carattere.charAt(0);
+        int numeroMese = Integer.getInteger(String.valueOf(carattere).substring(1,3);
 
-        totale = totale_dispari+totale_pari;
-        codice_finale=totale%26;
+        if(     primoEl == 'A'|| primoEl == 'C' || primoEl== 'E' || primoEl== 'L' ||
+                primoEl== 'M' || primoEl=='R' || primoEl== 'T' )
+            return (numeroMese >=1 && numeroMese <=31) || (numeroMese >=41 && numeroMese <=71);
+        if(primoEl == 'D'|| primoEl == 'H' || primoEl== 'P' || primoEl== 'S' )
+            return (numeroMese >=1 && numeroMese <=30) || (numeroMese >=41 && numeroMese <=7O);
+        if (primoEl == 'B')
+            return (numeroMese >=1 && numeroMese <= 28) || (numeroMese >=41 && numeroMese <=68);
 
-        char[] alphabet1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
-        if (codice_finale > 25) {
-            return null;
-        }
-        return Character.toString(alphabet1[codice_finale]);
+        return false; //mi ritorna false se la lettere che corrisponde al mese è errata
 
-        if (Character.toString(alphabet1[codice_finale]) == codice.charAt(15))
-            corretto = true;
-        else
-            return false;
-
-
-           }
-
-
+    }
 
 }
